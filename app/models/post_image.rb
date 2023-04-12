e462cbc9f269db32af32fd78a9cd71a75006ceff
EC2 Default User <ec2-user@ip-172-31-44-47.ap-northeast-1.compute.internal>
2023-04-12 06:58:07 +0000

    13章まで完

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,6 +1,15 @@
 class PostImage < ApplicationRecord
-  
+
   has_one_attached :image
   belongs_to :user
   
+  
+  def get_image#画像が設定されない場合はapp/assets/imagesに格納されているno_image.jpgという画像をデフォルト画像としてActiveStorageに格納し、格納した画像を表示する
+    unless image.attached?#ifのfalseバージョン
+      file_path = Rails.root.join('app/assets/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default_image.jpg',content_type: 'image/jpeg')#attach=付ける
+    end
+    image
+  end
+
 end
